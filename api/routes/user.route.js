0f238012099d22503f2cb684bc5005bafd6ffd19
ARAVIND SK <ask.aravind1015@gmail.com>
2024-10-05 15:49:43 +0530

    added delete account functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { signout, test, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, signout, test, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.get("/test", test);
 router.post("/signout",verifyToken, signout);
 router.put("/update/:userId",verifyToken, updateUser);
+router.delete("/delete/:userId",verifyToken, deleteUser);
 
 export default router;
